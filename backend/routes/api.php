9ba0bce9ade1b559116564d406ae8d7196753250
Bick Renan <renanbickdev@gmail.com>
2024-06-18 00:57:02 -0300

    EXCLUSÃO DO BACK

--- backend/routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-use App\Http\Controllers\ItemsController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::apiResource('itens', 'ItemsController', ['only' => ['index', 'create', 'delete', 'update' ]]);
