f53d47878def388d5affa2e76c980661641365d1
RenanBick <renanbickdev@gmail.com>
2024-06-17 21:51:54 -0300

    rotas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,9 +3,4 @@ use App\Http\Controllers\ItemsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/produtos', function (Request $request) {});
-Route::post('/produtos', function (Request $request) {});
-Route::put('/produtos/{id}', function (Request $request) {});
-Route::delete('/produtos/{id}', function (Request $request) {});
-
 Route::apiResource('itens', 'ItemsController', ['only' => ['index', 'create', 'delete', 'update' ]]);
