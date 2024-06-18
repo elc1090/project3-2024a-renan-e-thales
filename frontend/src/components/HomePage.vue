bd8f799a55f738805b1e5e3fba9c3a60c5a63a06
Thales Lagemann <thalesalaugustol@gmail.com>
2024-06-18 11:00:17 -0300

    .

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -63,13 +63,7 @@ export default defineComponent({
         const items = ref<Item[]>([]);
 
         const ItemsList = ref<Item[]>([
-            new Item('Garrafa de Água', 26, '500ml', 'sem descrição', 'suprimento', '10-10-10:01:02'),
-            new Item('Pacote de Sal', 128, '1Kg', 'pacote de sal', 'suprimento', '10-10-10:01:02'),
-            new Item('Pacote de Arroz', 256, '2Kg', 'pacote de arroz', 'suprimento', '10-10-10:01:02'),
-            new Item('Lata de Milho', 256, '300g', 'Lata de Milho', 'suprimento', '10-10-10:01:02'),
-            new Item('Garrafa de Suco de Uva', 256, '2L', 'Garrafa de Suco de Uva', 'suprimento', '10-10-10:01:02'),
-            new Item('Pacote de Farinha', 256, '1Kg', 'Pacote de Farinha', 'suprimento', '10-10-10:01:02'),
-            new Item('Pote de Café', 256, '250g', 'Pote de Café', 'suprimento', '10-10-10:01:02'),
+
         ]);
 
         async function fetchItems() {
@@ -85,6 +79,14 @@ export default defineComponent({
                     .catch(error => {
                         console.error('Erro ao fazer a chamada para a API:', error);
                     });
+
+                ItemsList.value.push(new Item('Garrafa de Água', 26, '500ml', 'sem descrição', 'suprimento', '10-10-10:01:02'),
+                    new Item('Pacote de Sal', 128, '1Kg', 'pacote de sal', 'suprimento', '10-10-10:01:02'),
+                    new Item('Pacote de Arroz', 256, '2Kg', 'pacote de arroz', 'suprimento', '10-10-10:01:02'),
+                    new Item('Lata de Milho', 256, '300g', 'Lata de Milho', 'suprimento', '10-10-10:01:02'),
+                    new Item('Garrafa de Suco de Uva', 256, '2L', 'Garrafa de Suco de Uva', 'suprimento', '10-10-10:01:02'),
+                    new Item('Pacote de Farinha', 256, '1Kg', 'Pacote de Farinha', 'suprimento', '10-10-10:01:02'),
+                    new Item('Pote de Café', 256, '250g', 'Pote de Café', 'suprimento', '10-10-10:01:02'),);
                 loadingStore.setLoading(false);
 
             } catch (error) {
